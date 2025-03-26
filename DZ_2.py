b6725e0c5f8e58d5bcf3e4fdcf9b7f0c9d23eadc
Vlad <vladkuznecov271@gmail.com>
2025-03-26 11:58:59 +0200

    Update DZ_2.py

--- DZ_2.py
+++ DZ_2.py
@@ -2,10 +2,14 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     try:
-        get_ticket= sorted(random.sample(range(min, max), quantity))
-        return get_ticket
-    except ValueError:
+        if max < 1000:
+            if quantity < max - min:
+                return []
+            get_ticket= sorted(random.sample(range(min, max), quantity))
+            return get_ticket
+        else:
+            return []
+    except ValueError or NameError:
         return []
 
-print(get_numbers_ticket(1000, 1002, 3))
-
+print(get_numbers_ticket(1000, 999, 1))

5c24da6acd934f270efa1781e02e327420325646
Vlad <vladkuznecov271@gmail.com>
2025-03-27 17:10:06 +0200

    Update DZ_2.py

--- DZ_2.py
+++ DZ_2.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     try:
-        if max < 1000:
+        if max < 1000 and min>=1:
             get_ticket= sorted(random.sample(range(min, max), quantity))
             return get_ticket
         else:
